2112471cb91adc4a5595a5ed7a05ee6b36badabe
Ilya Antonov <ilya.anton0v@vk.com>
2023-03-29 15:15:13 +0300

    update build in labs 4 & 5

--- DynamicStruct_Lab4/build/cmake_install.cmake
+++ DynamicStruct_Lab4/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ilyaantonov/Downloads/2 курс/Программирование ДС/DynamicStruct_Lab4
+# Install script for directory: /Users/ilyaantonov/Downloads/2 курс/PDSD/DynamicStruct_Lab4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/ilyaantonov/Downloads/2 курс/Программирование ДС/DynamicStruct_Lab4/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ilyaantonov/Downloads/2 курс/PDSD/DynamicStruct_Lab4/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
